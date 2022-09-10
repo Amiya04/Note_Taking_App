b182eaeea44cd4d04d414c7ce200b746cd30687e
Amiya Karmakar <79296886+Amiya04@users.noreply.github.com>
2022-09-10 09:26:33 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>Note</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a
+        lobortis sem, vel consectetur tellus. Phasellus venenatis in quam non
+        vestibulum. Suspendisse sit amet pretium sem. Vivamus sit amet accumsan
+        velit, lacinia tincidunt lorem.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
